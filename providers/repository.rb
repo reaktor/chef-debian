5792bbdacb620a6f45417cf9f3424ae428a3e746
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2013-01-21 00:37:23 -0300

    Run `apt-get update` on apt_repository updates
    Workaround for apt cookbook v1.8.0 bug introduced by
    http://tickets.opscode.com/browse/COOK-2171
    
    Fixes #3.

--- providers/repository.rb
+++ providers/repository.rb
@@ -48,7 +48,14 @@ def repository_resource(exec_action)
     action       :nothing
   end
   r.run_action exec_action
-  new_resource.updated_by_last_action(true) if r.updated_by_last_action?
+  if r.updated_by_last_action?
+    new_resource.updated_by_last_action(true)
+    # Run `apt-get update` ourself as a workaround for apt cookbook bug
+    # introduced by http://tickets.opscode.com/browse/COOK-2171
+    execute "apt-get update" do
+      action :run
+    end
+  end
 end
 
 def preferences_resourse(exec_action)
