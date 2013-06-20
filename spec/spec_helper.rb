7676f8e9605c63efaffd9a57a755c5fcb8fcf74e
Teemu Matilainen <teemu.matilainen@reaktor.fi>
2013-06-20 16:33:51 -0300

    Add unit tests for library methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,25 @@
 require 'berkshelf'
 require 'chefspec'
+require 'pathname'
 
-Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each { |f| require f }
+spec_root     = Pathname(__FILE__).parent
+cookbook_root = spec_root.parent
+
+# Load helpers
+Dir["#{spec_root}/support/**/*.rb"].each { |f| require f }
+
+# Add cookbook's libraries/ to the load path
+$:.unshift(cookbook_root.join('libraries'))
 
 Berkshelf.ui.mute!
-berksfile = File.expand_path('../../Berksfile', __FILE__)
+berksfile = cookbook_root.join('Berksfile')
 berks = Berkshelf::Berksfile.from_file(berksfile)
 berks.install(path: 'vendor/cookbooks', except: 'integration')
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
   config.color = true
   config.tty = true
 end
